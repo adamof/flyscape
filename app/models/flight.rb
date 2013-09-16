3d83f69c48be01f2c35ec3ec979e5b3b4750fe65
Stefan Adamov <stefan.adamof@gmail.com>
2013-09-16 23:09:46 +0100

    flight class

---
+++ app/models/flight.rb
@@ -0,0 +1,11 @@
+class Flight
+  attr_reader :from_city, :from_airport,
+              :to_cty, :to_airport,
+              :price, :date
+
+  def initialize args
+    args.each do |k,v|
+      instance_variable_set("@#{k}", v) unless v.nil?
+    end
+  end
+end
