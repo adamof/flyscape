ec90bbc099f35d7539ee8af64eaf734b33adcc6d
koshov <koshov@gmail.com>
2013-10-19 17:56:04 +0100

    Front end HTML + SCSS

--- front_end/Gruntfile.js
+++ front_end/Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
         },
         connect: {
             options: {
-                port: 3000,
+                port: 8080,
                 livereload: 35729,
                 // change this to '0.0.0.0' to access the server from outside
                 hostname: '0.0.0.0'
